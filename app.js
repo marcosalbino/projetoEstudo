0080259b4d99208f873f1fa4354bcdc8db7af263
marcosalbino <68089913+marcosalbino@users.noreply.github.com>
2024-09-07 10:33:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 function pesquisar() {
-    // Seleciona a seção HTML onde os resultados serão exibidos
-    let section = document.getElementById("resultados-pesquisa");
-
-    // Inicializa uma string vazia para armazenar os resultados HTML
-    let resultados = "";
-
-    // Itera sobre cada elemento (dado) do array de dados
-    for (let dado of dados) {
-        // Constrói a estrutura HTML de cada resultado
-        resultados += `
+  // Seleciona a seção HTML onde os resultados serão exibidos
+  let section = document.getElementById("resultados-pesquisa");
+  //
+  let campoPesquisa = document.getElementById("campo-pesquisa").value.toLowerCase()
+  //
+  if (campoPesquisa === "") {
+    section.innerHTML = "<p>Nada foi encontrado! Digite algo a ser pesquisado!</p>";
+    return
+  }
+  //
+  // Inicializa uma string vazia para armazenar os resultados HTML
+  let resultados = "";
+  //
+  let tituloDescricaoTags = ""
+  // Itera sobre cada elemento (dado) do array de dados
+  for (let dado of dados) {
+    //
+    tituloDescricaoTags = dado.titulo.toLowerCase() + dado.descricao.toLowerCase() + dado.tags.toLowerCase();
+    //
+    if (tituloDescricaoTags.includes(campoPesquisa)) {
+      // Constrói a estrutura HTML de cada resultado
+      resultados += `
         <div class="item-resultado">
           <h2>
             <a href="${dado.link}" target="_blank">${dado.titulo}</a>
@@ -18,7 +30,11 @@ function pesquisar() {
         </div>
       `;
     }
-
-    // Atribui a string com os resultados ao conteúdo HTML da seção
-    section.innerHTML = resultados;
+  }
+  //
+  // Atribui a string com os resultados ao conteúdo HTML da seção
+  if (!resultados) {
+    resultados = "<p>Sem informaçãoes a serem exibidas!</p>";
+  }
+  section.innerHTML = resultados;
 }
